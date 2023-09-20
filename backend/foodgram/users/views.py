bb6b109c0d7c423d8b5de4c00f952478c753df7b
Lozhkin-pa <wsl-user@LAPTOP-12385HBU>
2023-09-21 00:01:39 +0400

    Добавлены докстринги

--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -1,79 +1,78 @@
 from djoser.views import UserViewSet
-from .serializers import CustomUserSerializer, SubscriptionsSerializer, SubscriptionsListSerializer
+from .serializers import SubscriptionsSerializer
 from .models import Subscriptions, User
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.pagination import PageNumberPagination
 from django.shortcuts import get_object_or_404
 
 
-# class FollowViewSet(UserViewSet):
-#     serializer_class = SubscriptionsSerializer
-
-#     def get_queryset(self):
-#         return Subscriptions.objects.filter(user=self.request.user)
-
-#     def perform_create(self, serializer):
-#         serializer.save(user=self.request.user)
-
-
 class CustomUserViewSet(UserViewSet):
-    # queryset = User.objects.all()
-    pagination_class = PageNumberPagination
+    """
+    Переопределен вьюсет из djoser.
+    Добавлены специальные маршрутизируемые методы для работы с подписками.
+    """
 
-    @action(
-        detail=True,
-        methods=['post', 'delete'],
-    )
+    @action(detail=True, methods=['post', 'delete'])
     def subscribe(self, request, id):
+        """
+        Подписаться/отписаться от пользователя.
+        """
         author = get_object_or_404(User, id=id)
-        
         if request.user == author:
             return Response(
                 {'errors': 'Действия с собственным профилем невозможны!'},
                 status=status.HTTP_400_BAD_REQUEST,
             )
-
         if request.method == 'POST':
-            if Subscriptions.objects.filter(user=request.user, author=author).exists():
+            if Subscriptions.objects.filter(
+                user=request.user,
+                author=author
+            ).exists():
                 return Response(
                     {'errors': 'Вы уже подписаны на данного пользователя!'},
                     status=status.HTTP_400_BAD_REQUEST
                 )
-            subscription_obj = Subscriptions.objects.create(user=request.user, author=author)
-
-            serializer = SubscriptionsListSerializer(
-                subscription_obj, data=request.data, context={'request': request}
+            subscriptions_obj = Subscriptions.objects.create(
+                user=request.user,
+                author=author
+            )
+            serializer = SubscriptionsSerializer(
+                subscriptions_obj, 
+                data=request.data,
+                context={'request': request}
             )
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-
         if request.method == 'DELETE':
-            if not Subscriptions.objects.filter(user=request.user, author=author).exists():
+            if not Subscriptions.objects.filter(
+                user=request.user,
+                author=author
+            ).exists():
                 return Response(
                     {'errors': 'Вы не подписаны на данного пользователя!'},
                     status=status.HTTP_400_BAD_REQUEST
                 )
-            subscription_obj = get_object_or_404(
-                Subscriptions, user=request.user, author=author
+            subscriptions_obj = get_object_or_404(
+                Subscriptions,
+                user=request.user,
+                author=author
             )
-            subscription_obj.delete()
+            subscriptions_obj.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
 
-    @action(detail=False,
-    )
+    @action(detail=False)
     def subscriptions(self, request):
-        user = request.user
-        # queryset = user.subscriptions.all()
-        queryset = Subscriptions.objects.filter(user=user)
-        # subscriptions = User.objects.filter(subscribers__user=request.user)
-        pages = self.paginate_queryset(queryset)
-        serializer = SubscriptionsListSerializer(pages, many=True, context={'request': request})
+        """
+        Возвращает пользователей (авторов), на которых подписан текущий
+        пользователь.
+        """
+        subscriptions_list = Subscriptions.objects.filter(user=request.user)
+        pages = self.paginate_queryset(subscriptions_list)
+        serializer = SubscriptionsSerializer(
+            pages,
+            many=True,
+            context={'request': request}
+        )
         return self.get_paginated_response(serializer.data)
-    
-        # obj = User.objects.filter(author=request.user)
-        # serializer = SubscriptionsListSerializer(data=obj)
-        # serializer.is_valid()
-        # return Response(serializer.data)
