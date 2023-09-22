441051424a8714d57bd4ec9f49b7958203256099
Lozhkin-pa <wsl-user@LAPTOP-12385HBU>
2023-09-22 21:21:39 +0400

    Добавлены Избранное и Корзина

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -39,8 +39,22 @@ class TagRecipeAdmin(admin.ModelAdmin):
     search_fields = ('recipe__name', 'tag__name')
     list_filter = ('recipe__name', 'tag__name')
 
+
+class FavoritesAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'recipe',)
+    search_fields = ('recipe__name', 'user__username')
+    list_filter = ('recipe__name', 'user__username')
+
+
+class Shopping_cartAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'recipe',)
+    search_fields = ('recipe__name', 'user__username')
+    list_filter = ('recipe__name', 'user__username')
+
 admin.site.register(Recipe, RecipesAdmin)
 admin.site.register(Tag, TagsAdmin)
 admin.site.register(Ingredient, IngredientsAdmin)
 admin.site.register(IngredientRecipe, IngredientRecipeAdmin)
 admin.site.register(TagRecipe, TagRecipeAdmin)
+admin.site.register(Favorite, FavoritesAdmin)
+admin.site.register(Shopping_cart, Shopping_cartAdmin)
