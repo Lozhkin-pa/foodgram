b154891c80d85ddcd72a97b82542022a12763558
Lozhkin-pa <wsl-user@LAPTOP-12385HBU>
2023-09-22 12:40:01 +0400

    Добавлены вьюшки теги, ингредиенты, рецепты

--- backend/foodgram/recipes/views.py
+++ backend/foodgram/recipes/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
+from .models import Tag, Ingredient, Recipe, TagRecipe, IngredientRecipe, Favorite, Shopping_cart
+from .serializers import TagSerializer, IngredientSerializer
 
-# Create your views here.
+
+class ListRetrieveViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet
+):
+    pass
+
+
+class TagViewSet(ListRetrieveViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class IngredientViewSet(ListRetrieveViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
