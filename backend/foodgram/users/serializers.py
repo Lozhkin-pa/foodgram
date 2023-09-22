cdf6e7c1cfb75844088e1c4e1f0391424146f4e5
Lozhkin-pa <wsl-user@LAPTOP-12385HBU>
2023-09-22 21:24:09 +0400

    SubscriptionsSerializer вынес в отдельный файл

--- backend/foodgram/users/serializers.py
+++ backend/foodgram/users/serializers.py
@@ -1,8 +1,6 @@
 from djoser.serializers import UserSerializer, UserCreateSerializer
 from rest_framework import serializers
 from .models import User, Subscriptions
-from recipes.models import Recipe
-from recipes.serializers import RecipeSerializer
 
 
 class CustomUserSerializer(UserSerializer):
@@ -44,48 +42,3 @@ class CustomUserCreateSerializer(UserCreateSerializer):
             'last_name',
             'password'
         )
-
-
-class SubscriptionsSerializer(serializers.ModelSerializer):
-    """
-    Данные пользователя (автора), на которого подписан текущий пользователь.
-    В выдачу добавляются рецепты.
-    """
-    email = serializers.ReadOnlyField(source='author.email')
-    id = serializers.ReadOnlyField(source='author.id')
-    username = serializers.ReadOnlyField(source='author.username')
-    first_name = serializers.ReadOnlyField(source='author.first_name')
-    last_name = serializers.ReadOnlyField(source='author.last_name')
-    is_subscribed = serializers.SerializerMethodField(read_only=True)
-    recipes = serializers.SerializerMethodField(read_only=True)
-    recipes_count = serializers.SerializerMethodField(read_only=True)
-
-    class Meta:
-        model = Subscriptions
-        fields = (
-            'email',
-            'id',
-            'username',
-            'first_name',
-            'last_name',
-            'is_subscribed',
-            'recipes',
-            'recipes_count'
-        )
-    
-    def get_is_subscribed(self, obj):
-        request = self.context.get('request')
-        return Subscriptions.objects.filter(
-            user=request.user,
-            author=obj.author
-        ).exists()
-    
-    def get_recipes(self, obj):
-        queryset = Recipe.objects.filter(author=obj.author)
-        serializer = RecipeSerializer(queryset, many=True)
-        return serializer.data
-    
-    def get_recipes_count(self, obj):
-        recipes = Recipe.objects.filter(author=obj.author)
-        return recipes.count()
-
