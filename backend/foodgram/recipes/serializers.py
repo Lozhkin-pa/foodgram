7e0f4aaa61f132c6cfb37d16e0c50b6b5a961355
Lozhkin-pa <wsl-user@LAPTOP-12385HBU>
2023-09-22 12:39:05 +0400

    Добавлены сериализаторы теги, ингредиенты, рецепты

---
+++ backend/foodgram/recipes/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import Tag, Ingredient, Recipe, TagRecipe, IngredientRecipe, Favorite, Shopping_cart
+
+User = get_user_model()
+
+
+class TagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = (
+            'id',
+            'name',
+            'color',
+            'slug',
+        )
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Ingredient
+        fields = (
+            'id',
+            'name',
+            'measurement_unit',
+        )
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id',
+            'name',
+            'image',
+            'cooking_time'
+        )
